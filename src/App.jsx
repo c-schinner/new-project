447f612ca7b0dd75bbb2c0df13a5f48b5b86f591
Chris <cschinner7@gmail.com>
2025-01-14 12:41:20 -0800

    adding a nice carousel of pictures to the site

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,22 @@
+import Carousel from "./components/Carousel";
+import CarouselButtons from "./components/CarouselButtons";
 import Navbar from "./components/Navbar";
 
 function App() {
     return (
         <>
             <div>
-                <h1 className="text-center text-4xl font-bold text-black">
+                <h1 className="text-center text-4xl font-bold text-white">
                     hello new project
                 </h1>
                 <Navbar />
             </div>
-            <div></div>
+            <div>
+                <Carousel />
+            </div>
+            <div>
+                <CarouselButtons />
+            </div>
         </>
     );
 }
