ca124cb8aafe5261e8233f8a635d91f8cc708ca9
Chris <cschinner7@gmail.com>
2025-01-14 12:33:10 -0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+        <>
+            <div>
+                <h1 className="text-center text-4xl font-bold text-black">
+                    hello new project
+                </h1>
+                <Navbar />
+            </div>
+            <div></div>
+        </>
+    );
+}
+
+export default App;
